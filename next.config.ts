2734cf717a0f50ecca2d560454d9916ed6d8b747
Rieko00 <ariekbudianto@gmail.com>
2026-05-07 10:34:40 +0700

    fix onnx

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,10 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ["onnxruntime-node", "sharp", "dicom-parser"],
   outputFileTracingIncludes: {
     '/api/**/*': [
-      './node_modules/**/*.so',
-      './node_modules/**/*.so.*',
-      './node_modules/**/*.dylib',
-      './node_modules/**/*.dll',
+      './node_modules/onnxruntime-node/bin/napi-v3/linux/x64/**/*.so',
+      './node_modules/onnxruntime-node/bin/napi-v3/linux/x64/**/*.so.*',
+      './node_modules/onnxruntime-node/bin/napi-v6/linux/x64/**/*.so',
+      './node_modules/onnxruntime-node/bin/napi-v6/linux/x64/**/*.so.*',
       './models/**/*'
     ],
   },
